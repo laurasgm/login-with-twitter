ab0b3f2ae8033f215f3e9af375fc8bff79621b54
laurasgm <laurasophia.gonzalez@utp.edu.co>
2019-10-16 00:59:56 -0500

    inicio de sesion con twitter y google

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
 
-function App() {
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCvSgLDKC8A-iIKoRH3fyVC49bqvkR2ZgU",
+  authDomain: "loginwithsocial-e6771.firebaseapp.com"
+})
+
+class App extends React.Component {
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+ 
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Twitter as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount= () => {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        user => { this.setState({isSignedIn: !!user})
+        console.log("user", user)
+        });
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.isSignedIn ? (
+        <span>
+        <h1>Inicio de sesion exitoso!</h1>
+        <p>Bienvenido(a) {firebase.auth().currentUser.displayName}! has iniciado sesion!</p>
+        <img
+          alt="Foto de Perfil" 
+          src={firebase.auth().currentUser.photoURL}/>
+        <button onClick={() => firebase.auth().signOut()}>Sign-out</button> </span>
+      ):(
+        <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+      )}
     </div>
-  );
+  )
 }
-
+}
 export default App;
